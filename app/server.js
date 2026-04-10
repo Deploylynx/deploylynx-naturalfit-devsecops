f1c0116f758942869fd1ce083fd30cca467e7f34
Hina-Atif <hinaatif355@gmail.com>
2026-04-10 23:11:33 +0500

    fix: correctly path to public/index.html

--- app/server.js
+++ app/server.js
@@ -3,12 +3,12 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// This line tells the server to look in the current folder for your HTML/CSS
-app.use(express.static(__dirname)); 
+// 1. Tell Express to serve images/CSS from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-// This handles the main route
+// 2. Tell the server to find index.html INSIDE the 'public' folder
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, '0.0.0.0', () => {
